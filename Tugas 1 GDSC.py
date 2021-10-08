0242861636b2b7f84dd2112e796fe249105a7ed1
Komang Ryan <kryandhi@gmail.com>
2021-10-08 15:46:42 +0700

    koawowkoakawkoawokws

---
+++ Tugas 1 GDSC.py
@@ -0,0 +1,3 @@
+print('Nama : Komang Ryandhi Suandita')
+print('Jurusan : Matematika')
+print('Alasan ingin masuk gdsc : Ingin memperdalam hardskill, Memperluas koneksi di luar kampus dan Tentunya karena gdsc itb itu paling kece')
